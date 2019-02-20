3ede11b3f62d94b8a931711639fd2505da078304
Laurence Dodier <laurence.dodier@gmail.com>
2019-02-20 11:56:02 -0500

    switch case et hover sur tout les projets

--- module_js/afficheProjet.js
+++ module_js/afficheProjet.js
@@ -1,8 +1,13 @@
 (function(){
     var projet = document.querySelectorAll(".projet");
 
-    projet[0].addEventListener("click", function(){afficheProjet("devJeu")});
-    projet[1].addEventListener("click", function(){afficheProjet("progWeb")});
+    projet[0].addEventListener("click", function(){afficheProjet("1gauche")});
+    projet[1].addEventListener("click", function(){afficheProjet("2gauche")});
+    projet[2].addEventListener("click", function(){afficheProjet("3gauche")});
+
+    projet[3].addEventListener("click", function(){afficheProjet("1droite")});
+    projet[4].addEventListener("click", function(){afficheProjet("2droite")});
+    projet[5].addEventListener("click", function(){afficheProjet("3droite")});
 
     var fermer = document.querySelector(".fermer");
     fermer.addEventListener("click", function(){fermerProjet()});
@@ -20,9 +25,9 @@ function afficheProjet(x){
 
     document.querySelector("#presentation").style.display="block";
     switch(x){
-        case "devJeu":
-            titre.innerHTML = "devJeu";
-            sousTitre.innerHTML = "sousTitre";
+        case "1gauche":
+            titre.innerHTML = "Le fol Espoir";
+            sousTitre.innerHTML = "Production Web";
             parag1.innerHTML = "parag1";
             parag2.innerHTML = "parag2";
             parag3.innerHTML = "parag3";
@@ -30,9 +35,45 @@ function afficheProjet(x){
            // oImg.src = oImgLondres;
             break;
             
-        case "progWeb":
-            titre.innerHTML = "progWeb";
-            sousTitre.innerHTML = "sousTitre";
+        case "2gauche":
+            titre.innerHTML = "Bilboquet Laurier";
+            sousTitre.innerHTML = "Design d'interface";
+            parag1.innerHTML = "parag1";
+            parag2.innerHTML = "parag2";
+            parag3.innerHTML = "parag3";
+            break;
+        
+        case "3gauche":
+            titre.innerHTML = "Bilboquet Laurier";
+            sousTitre.innerHTML = "Propriétaire";
+            parag1.innerHTML = "parag1";
+            parag2.innerHTML = "parag2"; 
+            parag3.innerHTML = "parag3";
+            
+           // oImg.src = oImgLondres;
+            break;
+            
+        case "1droite":
+            titre.innerHTML = "Sphaera ⋄ Site Web";
+            sousTitre.innerHTML = "Developpement de jeu";
+            parag1.innerHTML = "parag1";
+            parag2.innerHTML = "parag2";
+            parag3.innerHTML = "parag3";
+            break;
+
+        case "2droite":
+            titre.innerHTML = "Sphaera ⋄ Jeu";
+            sousTitre.innerHTML = "Developpement de jeu";
+            parag1.innerHTML = "parag1";
+            parag2.innerHTML = "parag2";
+            parag3.innerHTML = "parag3";
+            
+           // oImg.src = oImgLondres;
+            break;
+            
+        case "3droite":
+            titre.innerHTML = "ChickenRun";
+            sousTitre.innerHTML = "Assemblage de jeu";
             parag1.innerHTML = "parag1";
             parag2.innerHTML = "parag2";
             parag3.innerHTML = "parag3";
