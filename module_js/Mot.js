837434ed119749243ad4d7e9794e23aa24c23bf1
Laurence <laurence.dodier@gmail.com>
2019-02-24 17:37:03 -0500

    ajout animation onclick mots

---
+++ module_js/Mot.js
@@ -0,0 +1,48 @@
+(function() {//IIFE
+	//----------On utilise un mode de programmation strict
+	"use strict";
+    console.log('Function iife');
+	/**
+	  * Classe permettant de créer et d'afficher des mots
+	  * @param {Number}      posX            - position du mot sur l'axe des X
+	  * @param {Number}      posY            - position du mot sur l'axe des Y
+	  * @param {String}      classeCSS       - chaîne indiquant la classe CSS à attribuer
+	  * @param {HTMLElement} conteneurParent - balise HTML pour afficher les mots animés
+	  * @param {Array}       lesMots         - Tableau des différents mots à afficher au hasard
+	  */ 
+	function Mot(posX, posY, lesMots, classeCSS, conteneurParent ) {
+		//Récupérer les valeurs passées en paramètres et les affecter à des propriétés directes de l'instance
+        this.posX = posX;
+        this.posY = posY;
+        this.classeCSS = classeCSS;
+        this.conteneurParent = conteneurParent;
+        this.lesMots = lesMots;
+		//Appeler la méthode d'instance pour créer et afficher le mot
+        this.creerMot();
+        console.log('fonction mot');
+	 }
+
+	 //Créer la méthode creerMot pour un objet de type Mot
+
+	Mot.prototype.creerMot = function() {
+        console.log('Mot.prototype.CreerMot');
+        //Le mot est un élément HTML  <p>
+		var leMot = document.createElement("p");
+        
+        leMot.style.left = this.posX + "px";
+        console.log("left " + this.posX);
+        leMot.style.top = this.posY + "px";
+        console.log("top " + this.posY);
+        leMot.innerHTML = this.lesMots;
+        leMot.classList.add(this.classeCSS);
+        
+        //Afficher l'élément  <div>, dans son conteneur parent
+		this.conteneurParent.appendChild(leMot);	
+    }
+	//Comme la fonction IIFE est anonyme,  ses variables, fonctions ou autres ne seront pas accessibles de l'extérieur
+	//Nous devons donc passer la "classe" Mot à l'objet HTML window comme propriété globale de notre application Web...
+    console.log("window.Mot :", window.Mot);
+	
+	window.Mot = Mot;
+
+})();//Fin IIFE
--- module_js/application.js
+++ module_js/application.js
@@ -1,39 +1,35 @@
+(function() {//IIFE
+	//----------On utilise un mode de programmation strict
+	"use strict";
+	console.log('Function application');
+	var laSection;//Balise HTML dans laquelle on va mettre des carrés
 
-import {contenuIntro} from './contenuIntro.js' // le contenu de l'intoduction */
-import {Introduction} from './Introduction.js' // 
-import {AnimLettre} from './AnimLettre.js' //
-
-
-/* l'élement de la page qui contiendra les éléments créés dynamiquement */
-let elmHeader = document.querySelector('.header')
-let intro = new Introduction(contenuIntro, elmHeader, animationLettre)
-
-// debutQuestionnaire()
-
-function animationLettre()
-{
-    /* Une fois que l'animation des mots est terminé la fonction animLettre s'exécutera */ 
-	console.log('debut animation lettre')
-	const lesLettres = 'LAURENCE DODIER'
- 	let  monAnimLettre = new AnimLettre(lesLettres, elmHeader, finAnim)	
-}
+	//On affecte le "mousedown" sur la section dès que la page HTML est chargée
+	window.addEventListener("load", function() {
+		//Identifier la section
+		laSection = document.querySelector("section");
+		console.log(laSection);
+		
+		//Lors d'un "mousedown" dans la section, on va instancier des mots...
+		laSection.addEventListener("mousedown", mettreDesMots, false);
 
 
-function finAnim()
-{
-	console.log('animation terminée')
-}
-
-
-
-
-
-
-
-
-
-
+	}, false);
 	
+	function mettreDesMots(evt) {
+		console.log(evt);
+		//Identifier où le mousedown a eu lieu dans la section
+		var posX = evt.clientX - 110;
+		var posY = evt.clientY - 180;
+		
+		var lesMots = ["Perseverance", "Déterminé", "Curieuse", "Ouverte d'esprit", "Empathique", "Ambitieuse", "Autonome", "Humaine", "Leader", "Minutieuse", "Positive", "Souriante"];
+        
+        var motHasard = lesMots[Math.floor(Math.random() * lesMots.length)];
+			
+		//Mettre un mot à cet endroit.....
+		//function Mot(posX, posY, classeCSS, conteneurParent, lesMots) {
 
+		var unMot = new Mot(posX, posY, motHasard, "motAnim", laSection);
+	}
 
-
+})();//Fin IIFE
